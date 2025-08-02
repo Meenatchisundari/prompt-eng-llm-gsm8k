7bdcb654bfd4f5484215996070db01d76e3dcfd7
Meenatchi Sundari Muthirulappan <85951411+Meenatchisundari@users.noreply.github.com>
2025-08-03 00:36:27 +0100

    Update dspy_few_shot.py

--- prompts/dspy_few_shot.py
+++ prompts/dspy_few_shot.py
@@ -1,24 +1,16 @@
 import dspy
 
-FEW_SHOT_EXAMPLES = """
-Q: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?
-A: I start with 15 trees and end with 21 trees. So 21 - 15 = 6. #### 6
+class FewShotSignature(dspy.Signature):
+    question = dspy.InputField(desc="A math problem")
+    answer = dspy.OutputField(desc="Answer with final number")
 
-Q: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
-A: Jason started with 20 and ended with 12. So 20 - 12 = 8. #### 8
-"""
-
-class FewShotDSPy(dspy.Signature):
-    question = dspy.InputField()
-    answer = dspy.OutputField(desc="Final numeric answer in format: #### [answer]")
-
-class FewShotModule(dspy.Module):
+class FewShotDSPy(dspy.Module):
     def __init__(self):
         super().__init__()
-        self.predict = dspy.Predict(FewShotDSPy)
+        self.predict = dspy.Predict(FewShotSignature, demos=[
+            FewShotSignature(question="Alice had 10 apples and ate 3. How many are left?", answer="#### 7"),
+            FewShotSignature(question="John bought 4 pens and then 2 more. How many total?", answer="#### 6")
+        ])
 
     def forward(self, question):
-        prompt = f"{FEW_SHOT_EXAMPLES}\nQ: {question}\nA:"
-        return self.predict(question=prompt)
-
-FewShotDSPy = FewShotModule()
+        return self.predict(question=question)
